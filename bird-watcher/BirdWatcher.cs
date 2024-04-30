06ff541de1ed921ccef2066781f67ca97238c566
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-04-30 11:37:05 -0500

    Solved bird watcher

--- bird-watcher/BirdWatcher.cs
+++ bird-watcher/BirdWatcher.cs
@@ -1,33 +1,42 @@
 using System;
 
 class BirdCount {
-    private int[] birdsPerDay;
+    private readonly int[] birdsPerDay;
 
     public BirdCount(int[] birdsPerDay) {
         this.birdsPerDay = birdsPerDay;
     }
 
-    public static int[] LastWeek() {
-        throw new NotImplementedException("Please implement the (static) BirdCount.LastWeek() method");
-    }
+    public static int[] LastWeek() => [0, 2, 5, 3, 7, 8, 4];
 
-    public int Today() {
-        throw new NotImplementedException("Please implement the BirdCount.Today() method");
-    }
+    public int Today() => birdsPerDay[^1];
 
-    public void IncrementTodaysCount() {
-        throw new NotImplementedException("Please implement the BirdCount.IncrementTodaysCount() method");
-    }
+    public void IncrementTodaysCount() => birdsPerDay[^1]++;
 
     public bool HasDayWithoutBirds() {
-        throw new NotImplementedException("Please implement the BirdCount.HasDayWithoutBirds() method");
+        foreach (var day in birdsPerDay) {
+            if (day == 0) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public int CountForFirstDays(int numberOfDays) {
-        throw new NotImplementedException("Please implement the BirdCount.CountForFirstDays() method");
+        var count = 0;
+        foreach (var day in birdsPerDay[..numberOfDays]) {
+            count += day;
+        }
+        return count;
     }
 
     public int BusyDays() {
-        throw new NotImplementedException("Please implement the BirdCount.BusyDays() method");
+        var count = 0;
+        foreach (var day in birdsPerDay) {
+            if (day >= 5) {
+                count++;
+            }
+        }
+        return count;
     }
 }
