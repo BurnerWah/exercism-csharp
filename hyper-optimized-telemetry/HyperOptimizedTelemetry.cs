316419f8ba04e8455e0424ddebd18d3532242501
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-04-30 13:44:30 -0500

    Improved readibility of ToBuffer

--- hyper-optimized-telemetry/HyperOptimizedTelemetry.cs
+++ hyper-optimized-telemetry/HyperOptimizedTelemetry.cs
@@ -2,14 +2,13 @@ using System;
 
 public static class TelemetryBuffer {
     public static byte[] ToBuffer(long reading) => reading switch {
-        >= 4_294_967_296 => [0xf8, .. BitConverter.GetBytes(reading)],
-        >= 2_147_483_648 => [0x4, .. BitConverter.GetBytes(reading)],
-        >= 65_536 => [0xfc, .. BitConverter.GetBytes(reading)],
+        > UInt32.MaxValue => [0xf8, .. BitConverter.GetBytes(reading)],
+        > Int32.MaxValue => [0x4, .. BitConverter.GetBytes(reading)],
+        > UInt16.MaxValue => [0xfc, .. BitConverter.GetBytes(reading)],
         >= 0 => [0x2, .. BitConverter.GetBytes(reading)],
-        >= -32_768 => [0xfe, .. BitConverter.GetBytes((short)reading), 0x0, 0x0, 0x0, 0x0, 0x0, 0x0],
-        >= -2_147_483_648 => [0xfc, .. BitConverter.GetBytes((int)reading), 0x0, 0x0, 0x0, 0x0],
-        >= -9_223_372_036_854_775_808 => [0xf8, .. BitConverter.GetBytes(reading)],
-        // _ => []
+        >= Int16.MinValue => [0xfe, .. BitConverter.GetBytes((short)reading), 0x0, 0x0, 0x0, 0x0, 0x0, 0x0],
+        >= Int32.MinValue => [0xfc, .. BitConverter.GetBytes((int)reading), 0x0, 0x0, 0x0, 0x0],
+        >= Int64.MinValue => [0xf8, .. BitConverter.GetBytes(reading)],
     };
 
     public static long FromBuffer(byte[] buffer) => buffer[0] switch {
