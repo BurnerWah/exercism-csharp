589289df8f8c5684abcef3da622d2adc3302c1cf
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-04-30 10:56:46 -0500

    Replaced giant blocks with switch expressions

--- football-match-reports/FootballMatchReports.cs
+++ football-match-reports/FootballMatchReports.cs
@@ -1,46 +1,24 @@
 using System;
 
 public static class PlayAnalyzer {
-    public static string AnalyzeOnField(int shirtNum) {
-        switch (shirtNum) {
-            case 1:
-                return "goalie";
-            case 2:
-                return "left back";
-            case 3:
-            case 4:
-                return "center back";
-            case 5:
-                return "right back";
-            case 6:
-            case 7:
-            case 8:
-                return "midfielder";
-            case 9:
-                return "left wing";
-            case 10:
-                return "striker";
-            case 11:
-                return "right wing";
-            default:
-                throw new ArgumentOutOfRangeException(nameof(shirtNum));
-        }
-    }
+    public static string AnalyzeOnField(int shirtNum) => shirtNum switch {
+        1 => "goalie",
+        2 => "left back",
+        3 or 4 => "center back",
+        5 => "right back",
+        6 or 7 or 8 => "midfielder",
+        9 => "left wing",
+        10 => "striker",
+        11 => "right wing",
+        _ => throw new ArgumentOutOfRangeException(nameof(shirtNum)),
+    };
 
-    public static string AnalyzeOffField(object report) {
-        switch (report) {
-            case int count:
-                return $"There are {count} supporters at the match.";
-            case string message:
-                return message;
-            case Injury injury:
-                return $"Oh no! {injury.GetDescription()} Medics are on the field.";
-            case Incident incident:
-                return incident.GetDescription();
-            case Manager manager:
-                return manager.Club == null ? manager.Name : $"{manager.Name} ({manager.Club})";
-            default:
-                throw new ArgumentException("Invalid input type");
-        }
-    }
+    public static string AnalyzeOffField(object report) => report switch {
+        int count => $"There are {count} supporters at the match.",
+        string message => message,
+        Injury injury => $"Oh no! {injury.GetDescription()} Medics are on the field.",
+        Incident incident => incident.GetDescription(),
+        Manager manager => manager.Club == null ? manager.Name : $"{manager.Name} ({manager.Club})",
+        _ => throw new ArgumentException("Invalid input type"),
+    };
 }
