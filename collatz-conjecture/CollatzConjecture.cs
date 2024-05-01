ad1bf159c9f14e1f4b462f7403b3af7743713438
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-05-01 18:21:32 -0500

    Finished collatz conjecture

--- collatz-conjecture/CollatzConjecture.cs
+++ collatz-conjecture/CollatzConjecture.cs
@@ -2,6 +2,14 @@ using System;
 
 public static class CollatzConjecture {
     public static int Steps(int number) {
-        throw new NotImplementedException("You need to implement this function");
+        if (number > 0) {
+            var steps = 0;
+            while (number > 1) {
+                number = number % 2 == 0 ? number / 2 : 3 * number + 1;
+                steps++;
+            }
+            return steps;
+        }
+        throw new ArgumentOutOfRangeException(nameof(number));
     }
 }
