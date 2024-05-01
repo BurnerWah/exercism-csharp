908b51523c40bac0873b596c6c24d382705d8537
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-05-01 13:58:07 -0500

    Solved sum of multiples

--- sum-of-multiples/SumOfMultiples.cs
+++ sum-of-multiples/SumOfMultiples.cs
@@ -1,8 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class SumOfMultiples {
-    public static int Sum(IEnumerable<int> multiples, int max) {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static int Sum(IEnumerable<int> multiples, int max) => (from n in Enumerable.Range(1, max - 1)
+                                                                   from m in multiples
+                                                                   where m > 0
+                                                                   where m < max
+                                                                   where n % m == 0
+                                                                   select n).Distinct().Sum();
 }
