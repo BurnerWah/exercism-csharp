f892d998f8dc404d237a494b58365139a57eb5e5
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-05-01 18:18:34 -0500

    Removed skip args

--- collatz-conjecture/CollatzConjectureTests.cs
+++ collatz-conjecture/CollatzConjectureTests.cs
@@ -1,41 +1,35 @@
 using System;
+
 using Xunit;
 
-public class CollatzConjectureTests
-{
+public class CollatzConjectureTests {
     [Fact]
-    public void Zero_steps_for_one()
-    {
+    public void Zero_steps_for_one() {
         Assert.Equal(0, CollatzConjecture.Steps(1));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Divide_if_even()
-    {
+    [Fact]
+    public void Divide_if_even() {
         Assert.Equal(4, CollatzConjecture.Steps(16));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Even_and_odd_steps()
-    {
+    [Fact]
+    public void Even_and_odd_steps() {
         Assert.Equal(9, CollatzConjecture.Steps(12));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Large_number_of_even_and_odd_steps()
-    {
+    [Fact]
+    public void Large_number_of_even_and_odd_steps() {
         Assert.Equal(152, CollatzConjecture.Steps(1000000));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Zero_is_an_error()
-    {
+    [Fact]
+    public void Zero_is_an_error() {
         Assert.Throws<ArgumentOutOfRangeException>(() => CollatzConjecture.Steps(0));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Negative_value_is_an_error()
-    {
+    [Fact]
+    public void Negative_value_is_an_error() {
         Assert.Throws<ArgumentOutOfRangeException>(() => CollatzConjecture.Steps(-15));
     }
 }
