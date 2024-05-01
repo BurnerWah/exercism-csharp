4d1c14ec8ca614bbe1c137d3cd29c0e6c73b6fcc
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-05-01 12:30:05 -0500

    Added basic solution for squeaky clean

--- squeaky-clean/SqueakyClean.cs
+++ squeaky-clean/SqueakyClean.cs
@@ -1,7 +1,20 @@
 using System;
+using System.Text;
 
 public static class Identifier {
     public static string Clean(string identifier) {
-        throw new NotImplementedException($"Please implement the (static) Identifier.Clean() method");
+        var sb = new StringBuilder();
+        for (int i = 0; i < identifier.Length; i++) {
+            if (Char.IsWhiteSpace(identifier[i])) {
+                sb.Append('_');
+            } else if (Char.IsControl(identifier[i])) {
+                sb.Append("CTRL");
+            } else if (i > 0 && identifier[i - 1] == '-') {
+                sb.Append(Char.ToUpper(identifier[i]));
+            } else if (Char.IsLetter(identifier[i]) && !Char.IsBetween(identifier[i], 'α', 'ω')) {
+                sb.Append(identifier[i]);
+            }
+        }
+        return sb.ToString();
     }
 }
