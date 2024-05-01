736fe160f0f5748c6850e3b73190232412f0db12
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-05-01 14:07:59 -0500

    Solved pangram

--- pangram/Pangram.cs
+++ pangram/Pangram.cs
@@ -1,7 +1,6 @@
 using System;
+using System.Linq;
 
 public static class Pangram {
-    public static bool IsPangram(string input) {
-        throw new NotImplementedException();
-    }
+    public static bool IsPangram(string input) => input.ToLower().Distinct().Count(Char.IsLetter) == 26;
 }
