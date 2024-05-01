5e2c5df4dd593397c40575f9d5ae721a9d75d088
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-05-01 16:02:20 -0500

    solved calculator conondrum

--- calculator-conundrum/CalculatorConundrum.cs
+++ calculator-conundrum/CalculatorConundrum.cs
@@ -1,7 +1,14 @@
 using System;
 
 public static class SimpleCalculator {
-    public static string Calculate(int operand1, int operand2, string operation) {
-        throw new NotImplementedException("Please implement the SimpleCalculator.Calculate() method");
-    }
+    // I am amazed this works
+    public static string Calculate(int operand1, int operand2, string operation) => (operation, operand2) switch {
+        ("+", _) => $"{operand1} {operation} {operand2} = {SimpleOperation.Addition(operand1, operand2)}",
+        ("*", _) => $"{operand1} {operation} {operand2} = {SimpleOperation.Multiplication(operand1, operand2)}",
+        ("/", 0) => "Division by zero is not allowed.",
+        ("/", _) => $"{operand1} {operation} {operand2} = {SimpleOperation.Division(operand1, operand2)}",
+        ("", _) => throw new ArgumentException(nameof(operation)),
+        (null, _) => throw new ArgumentNullException(nameof(operation)),
+        (_, _) => throw new ArgumentOutOfRangeException(nameof(operation)),
+    };
 }
