4d3280e848ab41778c1e3ec110e3990cd2b7be81
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-05-01 18:50:58 -0500

    Solved perfect numbers

--- perfect-numbers/PerfectNumbers.cs
+++ perfect-numbers/PerfectNumbers.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 public enum Classification {
     Perfect,
@@ -7,7 +8,15 @@ public enum Classification {
 }
 
 public static class PerfectNumbers {
-    public static Classification Classify(int number) {
-        throw new NotImplementedException();
-    }
+    public static Classification Classify(int number) => number switch {
+        <= 0 => throw new ArgumentOutOfRangeException(nameof(number)),
+        // An odd number must be at least 945 to be abundant number, so we can
+        // skip all te checks there
+        < 945 when Int32.IsOddInteger(number) => Classification.Deficient,
+        _ => (number - Enumerable.Range(1, number - 1).Where(n => number % n == 0).Sum()) switch {
+            > 0 => Classification.Deficient,
+            0 => Classification.Perfect,
+            < 0 => Classification.Abundant,
+        },
+    };
 }
