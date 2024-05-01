3dd12782f19010e972df62e5e9977a899a6325f8
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-05-01 13:29:21 -0500

    Switched to expression format for methods

--- space-age/SpaceAge.cs
+++ space-age/SpaceAge.cs
@@ -7,35 +7,19 @@ public class SpaceAge {
         this.seconds = seconds;
     }
 
-    public double OnEarth() {
-        return seconds / 31557600F;
-    }
+    public double OnEarth() => seconds / 31557600F;
 
-    public double OnMercury() {
-        return OnEarth() / 0.2408467;
-    }
+    public double OnMercury() => OnEarth() / 0.2408467;
 
-    public double OnVenus() {
-        return OnEarth() / 0.61519726;
-    }
+    public double OnVenus() => OnEarth() / 0.61519726;
 
-    public double OnMars() {
-        return OnEarth() / 1.8808158;
-    }
+    public double OnMars() => OnEarth() / 1.8808158;
 
-    public double OnJupiter() {
-        return OnEarth() / 11.862615;
-    }
+    public double OnJupiter() => OnEarth() / 11.862615;
 
-    public double OnSaturn() {
-        return OnEarth() / 29.447498;
-    }
+    public double OnSaturn() => OnEarth() / 29.447498;
 
-    public double OnUranus() {
-        return OnEarth() / 84.016846;
-    }
+    public double OnUranus() => OnEarth() / 84.016846;
 
-    public double OnNeptune() {
-        return OnEarth() / 164.79132;
-    }
+    public double OnNeptune() => OnEarth() / 164.79132;
 }
