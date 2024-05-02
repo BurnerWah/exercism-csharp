5376b84305598f1e5322cc1373837720857a5ae2
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-05-01 19:11:37 -0500

    Solved allergies

--- allergies/Allergies.cs
+++ allergies/Allergies.cs
@@ -1,25 +1,34 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
+[Flags]
 public enum Allergen {
-    Eggs,
-    Peanuts,
-    Shellfish,
-    Strawberries,
-    Tomatoes,
-    Chocolate,
-    Pollen,
-    Cats
+    Eggs = 1,
+    Peanuts = 2,
+    Shellfish = 4,
+    Strawberries = 8,
+    Tomatoes = 16,
+    Chocolate = 32,
+    Pollen = 64,
+    Cats = 128,
 }
 
-public class Allergies {
-    public Allergies(int mask) {
-    }
+public class Allergies(int mask) {
+    private readonly Allergen allergens = (Allergen)mask;
 
-    public bool IsAllergicTo(Allergen allergen) {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public bool IsAllergicTo(Allergen allergen) => allergens.HasFlag(allergen);
 
-    public Allergen[] List() {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    private static readonly List<Allergen> allergenFlags = [
+        Allergen.Eggs,
+        Allergen.Peanuts,
+        Allergen.Shellfish,
+        Allergen.Strawberries,
+        Allergen.Tomatoes,
+        Allergen.Chocolate,
+        Allergen.Pollen,
+        Allergen.Cats,
+    ];
+
+    public Allergen[] List() => allergenFlags.Where(IsAllergicTo).ToArray();
 }
