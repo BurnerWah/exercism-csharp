86b06e4728f96311e54c3d93ed54cd3f2aca21ba
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-05-01 19:14:24 -0500

    Added robot name

---
+++ robot-name/RobotName.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Robot {
+    public string Name {
+        get {
+            throw new NotImplementedException("You need to implement this function.");
+        }
+    }
+
+    public void Reset() {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+}
