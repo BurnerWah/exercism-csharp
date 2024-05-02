48a8a97071eee9bebf9df9187810f0992b56f2cf
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-05-02 13:18:58 -0500

    Solved triangle

--- triangle/Triangle.cs
+++ triangle/Triangle.cs
@@ -1,15 +1,19 @@
 using System;
 
 public static class Triangle {
-    public static bool IsScalene(double side1, double side2, double side3) {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static bool IsScalene(double side1, double side2, double side3) => true switch {
+        _ when side1 + side2 < side3 => false,
+        _ when side2 + side3 < side1 => false,
+        _ when side1 + side3 < side2 => false,
+        _ => side1 != side2 && side2 != side3 && side3 != side1,
+    };
 
-    public static bool IsIsosceles(double side1, double side2, double side3) {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static bool IsIsosceles(double side1, double side2, double side3) => true switch {
+        _ when side1 + side2 < side3 => false,
+        _ when side2 + side3 < side1 => false,
+        _ when side1 + side3 < side2 => false,
+        _ => side1 == side2 || side2 == side3 || side3 == side1,
+    };
 
-    public static bool IsEquilateral(double side1, double side2, double side3) {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static bool IsEquilateral(double side1, double side2, double side3) => side1 > 0 && side1 == side2 && side2 == side3;
 }
