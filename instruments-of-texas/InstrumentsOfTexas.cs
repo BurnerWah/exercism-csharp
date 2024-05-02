2e9a57b1f900ba151e14e0167288bb4ffc61792e
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-05-01 19:52:04 -0500

    Formatting

--- instruments-of-texas/InstrumentsOfTexas.cs
+++ instruments-of-texas/InstrumentsOfTexas.cs
@@ -1,32 +1,26 @@
 using System;
 
-public class CalculationException : Exception
-{
-    public CalculationException(int operand1, int operand2, string message, Exception inner)
+public class CalculationException : Exception {
     // TODO: complete the definition of the constructor
-    {
+    public CalculationException(int operand1, int operand2, string message, Exception inner) {
     }
 
     public int Operand1 { get; }
     public int Operand2 { get; }
 }
 
-public class CalculatorTestHarness
-{
+public class CalculatorTestHarness {
     private Calculator calculator;
 
-    public CalculatorTestHarness(Calculator calculator)
-    {
+    public CalculatorTestHarness(Calculator calculator) {
         this.calculator = calculator;
     }
 
-    public string TestMultiplication(int x, int y)
-    {
+    public string TestMultiplication(int x, int y) {
         throw new NotImplementedException("Please implement the CalculatorTestHarness.TestMultiplication() method");
     }
 
-    public void Multiply(int x, int y)
-    {
+    public void Multiply(int x, int y) {
         throw new NotImplementedException("Please implement the CalculatorTestHarness.Multiply() method");
     }
 }
@@ -35,12 +29,9 @@ public class CalculatorTestHarness
 // Please do not modify the code below.
 // If there is an overflow in the multiplication operation
 // then a System.OverflowException is thrown.
-public class Calculator
-{
-    public int Multiply(int x, int y)
-    {
-        checked
-        {
+public class Calculator {
+    public int Multiply(int x, int y) {
+        checked {
             return x * y;
         }
     }
