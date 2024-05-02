c8098270c3aa37f2f35b18650c2ab5b9960632b5
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-05-01 19:48:06 -0500

    Solved robot name

--- robot-name/RobotName.cs
+++ robot-name/RobotName.cs
@@ -1,13 +1,26 @@
 using System;
+using System.Collections.Generic;
 
 public class Robot {
-    public string Name {
-        get {
-            throw new NotImplementedException("You need to implement this function.");
-        }
-    }
+    public static HashSet<string> names = new HashSet<string>();
+
+    public Robot() => Name = GenerateName();
+
+    public string Name { get; private set; }
 
     public void Reset() {
-        throw new NotImplementedException("You need to implement this function.");
+        var newName = GenerateName();
+        names.Remove(Name);
+        Name = newName;
+    }
+
+    private static string GenerateName() {
+        var rng = new Random();
+        string newName;
+        do {
+            newName = $"{(char)rng.Next('A', 'Z' + 1)}{(char)rng.Next('A', 'Z' + 1)}{rng.Next(1000):D3}";
+        } while (names.Contains(newName));
+        names.Add(newName);
+        return newName;
     }
 }
