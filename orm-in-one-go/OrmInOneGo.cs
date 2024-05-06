7a8c975d9b616dc5eb7bdc06f7edd62e528a74d9
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-05-06 11:27:10 -0500

    Added ORM in one go

---
+++ orm-in-one-go/OrmInOneGo.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Orm {
+    private Database database;
+
+    public Orm(Database database) {
+        this.database = database;
+    }
+
+    public void Write(string data) {
+        throw new NotImplementedException($"Please implement the Orm.Write() method");
+    }
+
+    public bool WriteSafely(string data) {
+        throw new NotImplementedException($"Please implement the Orm.WriteSafely() method");
+    }
+}
