d42bac896aabc0913256786ab8e4517f16574f5f
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-05-06 15:30:32 -0500

    Solved parsing log files

--- parsing-log-files/ParsingLogFiles.cs
+++ parsing-log-files/ParsingLogFiles.cs
@@ -1,24 +1,36 @@
 using System;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 public class LogParser {
     public bool IsValidLine(string text) {
-        throw new NotImplementedException($"Please implement the LogParser.IsValidLine() method");
+        var re = new Regex(@"^\[(TRC|DBG|INF|WRN|ERR|FTL)\]");
+        return re.IsMatch(text);
     }
 
     public string[] SplitLogLine(string text) {
-        throw new NotImplementedException($"Please implement the LogParser.SplitLogLine() method");
+        var re = new Regex(@"<[\^*=\-]*>");
+        return re.Split(text);
     }
 
     public int CountQuotedPasswords(string lines) {
-        throw new NotImplementedException($"Please implement the LogParser.CountQuotedPasswords() method");
+        var re = new Regex("\".*password.*\"", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+        return re.Count(lines);
     }
 
     public string RemoveEndOfLineText(string line) {
-        throw new NotImplementedException($"Please implement the LogParser.RemoveEndOfLineText() method");
+        var re = new Regex(@"end-of-line\d+");
+        return re.Replace(line, "");
     }
 
     public string[] ListLinesWithPasswords(string[] lines) {
-        throw new NotImplementedException($"Please implement the LogParser.ListLinesWithPasswords() method");
+        var re = new Regex(@"^(.*)(password.*?)( .*$|$)", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+        var results = new List<string>();
+        foreach (var line in lines) {
+            results.Add(re.Replace(line, ReplacePassword));
+        }
+        return [.. results];
     }
+
+    private static string ReplacePassword(Match m) => m.Groups[2].Value.Equals("password", StringComparison.CurrentCultureIgnoreCase) ? $"--------: {m.Groups[0]}" : $"{m.Groups[2]}: {m.Groups[0]}";
 }
