8dfa67d4cc22e7451713ba6bd98883edc1880f4a
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-05-06 12:33:50 -0500

    Solved developer-privileges

--- developer-privileges/DeveloperPrivileges.cs
+++ developer-privileges/DeveloperPrivileges.cs
@@ -2,11 +2,33 @@ using System;
 using System.Collections.Generic;
 
 public class Authenticator {
-    // TODO: Implement the Authenticator.Admin property
-    public Identity Admin { get; }
+    public Identity Admin { get; } = new Identity {
+        Email = "admin@ex.ism",
+        NameAndAddress = ["Chanakya", "Mumbai", "India"],
+        FacialFeatures = new FacialFeatures {
+            EyeColor = "green",
+            PhiltrumWidth = 0.9m,
+        },
+    };
 
-    // TODO: Implement the Authenticator.Developers property
-    public IDictionary<string, Identity> Developers { get; }
+    public IDictionary<string, Identity> Developers { get; } = new Dictionary<string, Identity> {
+        ["Bertrand"] = new Identity {
+            Email = "bert@ex.ism",
+            NameAndAddress = ["Bertrand", "Paris", "France"],
+            FacialFeatures = new FacialFeatures {
+                EyeColor = "blue",
+                PhiltrumWidth = 0.8m,
+            },
+        },
+        ["Anders"] = new Identity {
+            Email = "anders@ex.ism",
+            NameAndAddress = ["Anders", "Redmond", "USA"],
+            FacialFeatures = new FacialFeatures {
+                EyeColor = "brown",
+                PhiltrumWidth = 0.85m,
+            },
+        },
+    };
 }
 
 //**** please do not modify the FacialFeatures class ****
