21e55c9b766540382be17a0d81aa17ee757ab8ca
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-05-06 12:01:30 -0500

    solved remote control competition

--- remote-control-competition/RemoteControlCompetition.cs
+++ remote-control-competition/RemoteControlCompetition.cs
@@ -1,32 +1,35 @@
 using System;
 using System.Collections.Generic;
 
-// TODO implement the IRemoteControlCar interface
+public interface IRemoteControlCar {
+    public int DistanceTravelled { get; }
+    public void Drive();
+}
 
-public class ProductionRemoteControlCar {
+public class ProductionRemoteControlCar : IRemoteControlCar, IComparable<ProductionRemoteControlCar> {
     public int DistanceTravelled { get; private set; }
     public int NumberOfVictories { get; set; }
 
-    public void Drive() {
-        DistanceTravelled += 10;
-    }
+    public int CompareTo(ProductionRemoteControlCar other) => NumberOfVictories - other.NumberOfVictories;
+
+    public void Drive() => DistanceTravelled += 10;
 }
 
-public class ExperimentalRemoteControlCar {
+public class ExperimentalRemoteControlCar : IRemoteControlCar {
     public int DistanceTravelled { get; private set; }
 
-    public void Drive() {
-        DistanceTravelled += 20;
-    }
+    public void Drive() => DistanceTravelled += 20;
 }
 
 public static class TestTrack {
-    public static void Race(IRemoteControlCar car) {
-        throw new NotImplementedException($"Please implement the (static) TestTrack.Race() method");
-    }
+    public static void Race(IRemoteControlCar car) => car.Drive();
 
-    public static List<ProductionRemoteControlCar> GetRankedCars(ProductionRemoteControlCar prc1,
-        ProductionRemoteControlCar prc2) {
-        throw new NotImplementedException($"Please implement the (static) TestTrack.GetRankedCars() method");
+    public static List<ProductionRemoteControlCar> GetRankedCars(
+        ProductionRemoteControlCar prc1,
+        ProductionRemoteControlCar prc2
+    ) {
+        var list = new List<ProductionRemoteControlCar> { prc1, prc2 };
+        list.Sort();
+        return list;
     }
 }
