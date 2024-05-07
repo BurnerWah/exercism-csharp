c0aea3f346b77d077f5a1f8be4d7ffd0c115e8a5
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-05-07 13:24:50 -0500

    Solved robot simulator

--- robot-simulator/RobotSimulator.cs
+++ robot-simulator/RobotSimulator.cs
@@ -7,29 +7,30 @@ public enum Direction {
     West
 }
 
-public class RobotSimulator {
-    public RobotSimulator(Direction direction, int x, int y) {
-    }
+public class RobotSimulator(Direction direction, int x, int y) {
+    private (Direction Direction, int X, int Y) position = (direction, x, y);
 
-    public Direction Direction {
-        get {
-            throw new NotImplementedException("You need to implement this method.");
-        }
-    }
-
-    public int X {
-        get {
-            throw new NotImplementedException("You need to implement this method.");
-        }
-    }
-
-    public int Y {
-        get {
-            throw new NotImplementedException("You need to implement this method.");
-        }
-    }
+    public Direction Direction => position.Direction;
+    public int X => position.X;
+    public int Y => position.Y;
 
     public void Move(string instructions) {
-        throw new NotImplementedException("You need to implement this method.");
+        foreach (var command in instructions) {
+            position = (Direction, command) switch {
+                (Direction.North, 'A') => (Direction, X, Y + 1),
+                (Direction.North, 'R') => (Direction.East, X, Y),
+                (Direction.North, 'L') => (Direction.West, X, Y),
+                (Direction.East, 'A') => (Direction, X + 1, Y),
+                (Direction.East, 'R') => (Direction.South, X, Y),
+                (Direction.East, 'L') => (Direction.North, X, Y),
+                (Direction.South, 'A') => (Direction, X, Y - 1),
+                (Direction.South, 'R') => (Direction.West, X, Y),
+                (Direction.South, 'L') => (Direction.East, X, Y),
+                (Direction.West, 'A') => (Direction, X - 1, Y),
+                (Direction.West, 'R') => (Direction.North, X, Y),
+                (Direction.West, 'L') => (Direction.South, X, Y),
+                _ => position,
+            };
+        }
     }
 }
