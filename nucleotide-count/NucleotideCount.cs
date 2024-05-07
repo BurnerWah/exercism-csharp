3506b7302c8c8495917acf08519040d53e4de36a
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-05-07 15:46:54 -0500

    Solved Nucleotide Count

--- nucleotide-count/NucleotideCount.cs
+++ nucleotide-count/NucleotideCount.cs
@@ -1,8 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 public static class NucleotideCount {
-    public static IDictionary<char, int> Count(string sequence) {
-        throw new NotImplementedException("You need to implement this method.");
-    }
+    public static IDictionary<char, int> Count(string sequence) => Regex.IsMatch(sequence, @"^[ACGT]*$")
+        ? (IDictionary<char, int>)new Dictionary<char, int> {
+            ['A'] = sequence.Count(c => c == 'A'),
+            ['C'] = sequence.Count(c => c == 'C'),
+            ['G'] = sequence.Count(c => c == 'G'),
+            ['T'] = sequence.Count(c => c == 'T'),
+        }
+        : throw new ArgumentException(nameof(sequence));
 }
