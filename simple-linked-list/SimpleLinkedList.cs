442b1c6ce517d087ec469f3db7661fa35d3ededc
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-05-07 13:55:47 -0500

    Added simple linked list

---
+++ simple-linked-list/SimpleLinkedList.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class SimpleLinkedList<T> {
+    public int Count => throw new NotImplementedException("You need to implement this method.");
+
+    public void Push(T value) {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+
+    public T Pop() {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+}
