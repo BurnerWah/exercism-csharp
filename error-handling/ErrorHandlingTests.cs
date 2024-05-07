94e2558d8669004420c08793d2c7be60485dec50
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-05-07 12:29:43 -0500

    Removed skip args

--- error-handling/ErrorHandlingTests.cs
+++ error-handling/ErrorHandlingTests.cs
@@ -1,21 +1,19 @@
 using System;
+
 using Xunit;
 
-public class ErrorHandlingTests
-{
+public class ErrorHandlingTests {
     // Read more about exception handling here:
     // https://msdn.microsoft.com/en-us/library/ms173162.aspx?f=255&MSPPError=-2147217396
     [Fact]
-    public void ThrowException()
-    {
+    public void ThrowException() {
         Assert.Throws<Exception>(() => ErrorHandling.HandleErrorByThrowingException());
     }
 
     // Read more about nullable types here:
     // https://msdn.microsoft.com/en-us/library/1t3y8s4s.aspx?f=255&MSPPError=-2147217396
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void ReturnNullableType()
-    {
+    [Fact]
+    public void ReturnNullableType() {
         var successfulResult = ErrorHandling.HandleErrorByReturningNullableType("1");
         Assert.Equal(1, successfulResult);
 
@@ -25,34 +23,30 @@ public class ErrorHandlingTests
 
     // Read more about out parameters here:
     // https://msdn.microsoft.com/en-us/library/t3c3bfhx.aspx?f=255&MSPPError=-2147217396
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void ReturnWithOutParameter()
-    {
+    [Fact]
+    public void ReturnWithOutParameter() {
         int result;
         var successfulResult = ErrorHandling.HandleErrorWithOutParam("1", out result);
         Assert.True(successfulResult);
         Assert.Equal(1, result);
-        
+
         var failureResult = ErrorHandling.HandleErrorWithOutParam("a", out result);
         Assert.False(failureResult);
         // The value of result is meaningless here (it could be anything) so it shouldn't be used and it's not validated 
     }
 
-    private class DisposableResource : IDisposable
-    {
+    private class DisposableResource : IDisposable {
         public bool IsDisposed { get; private set; }
 
-        public void Dispose()
-        {
+        public void Dispose() {
             IsDisposed = true;
         }
     }
 
     // Read more about IDisposable here:
     // https://msdn.microsoft.com/en-us/library/system.idisposable(v=vs.110).aspx
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void DisposableObjectsAreDisposedWhenThrowingAnException()
-    {
+    [Fact]
+    public void DisposableObjectsAreDisposedWhenThrowingAnException() {
         var disposableResource = new DisposableResource();
 
         Assert.Throws<Exception>(() => ErrorHandling.DisposableResourcesAreDisposedWhenExceptionIsThrown(disposableResource));
