45facf5c04baedd732290dd0744841d118aac971
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-05-07 11:06:24 -0500

    Simplified code

--- hyperia-forex/HyperiaForex.cs
+++ hyperia-forex/HyperiaForex.cs
@@ -1,66 +1,19 @@
 using System;
 
-public struct CurrencyAmount {
-    private decimal amount;
-    private string currency;
+public struct CurrencyAmount(decimal amount, string currency) {
+    private readonly decimal amount = amount;
+    private readonly string currency = currency;
 
-    public CurrencyAmount(decimal amount, string currency) {
-        this.amount = amount;
-        this.currency = currency;
-    }
-
-    public static bool operator ==(CurrencyAmount lhs, CurrencyAmount rhs) {
-        if (lhs.currency == rhs.currency) {
-            return lhs.amount == rhs.amount;
-        } else {
-            throw new ArgumentException();
-        }
-    }
+    public static bool operator ==(CurrencyAmount lhs, CurrencyAmount rhs) => lhs.currency == rhs.currency ? lhs.amount == rhs.amount : throw new ArgumentException();
     public static bool operator !=(CurrencyAmount lhs, CurrencyAmount rhs) => !(lhs == rhs);
 
-    public static bool operator >(CurrencyAmount lhs, CurrencyAmount rhs) {
-        if (lhs.currency == rhs.currency) {
-            return lhs.amount > rhs.amount;
-        } else {
-            throw new ArgumentException();
-        }
-    }
-    public static bool operator <(CurrencyAmount lhs, CurrencyAmount rhs) {
-        if (lhs.currency == rhs.currency) {
-            return lhs.amount < rhs.amount;
-        } else {
-            throw new ArgumentException();
-        }
-    }
+    public static bool operator >(CurrencyAmount lhs, CurrencyAmount rhs) => lhs.currency == rhs.currency ? lhs.amount > rhs.amount : throw new ArgumentException();
+    public static bool operator <(CurrencyAmount lhs, CurrencyAmount rhs) => lhs.currency == rhs.currency ? lhs.amount < rhs.amount : throw new ArgumentException();
 
-    public static CurrencyAmount operator +(CurrencyAmount lhs, CurrencyAmount rhs) {
-        if (lhs.currency == rhs.currency) {
-            return new CurrencyAmount(lhs.amount + rhs.amount, lhs.currency);
-        } else {
-            throw new ArgumentException();
-        }
-    }
-    public static CurrencyAmount operator -(CurrencyAmount lhs, CurrencyAmount rhs) {
-        if (lhs.currency == rhs.currency) {
-            return new CurrencyAmount(lhs.amount - rhs.amount, lhs.currency);
-        } else {
-            throw new ArgumentException();
-        }
-    }
-    public static CurrencyAmount operator *(CurrencyAmount lhs, CurrencyAmount rhs) {
-        if (lhs.currency == rhs.currency) {
-            return new CurrencyAmount(lhs.amount * rhs.amount, lhs.currency);
-        } else {
-            throw new ArgumentException();
-        }
-    }
-    public static CurrencyAmount operator /(CurrencyAmount lhs, CurrencyAmount rhs) {
-        if (lhs.currency == rhs.currency) {
-            return new CurrencyAmount(lhs.amount / rhs.amount, lhs.currency);
-        } else {
-            throw new ArgumentException();
-        }
-    }
+    public static CurrencyAmount operator +(CurrencyAmount lhs, CurrencyAmount rhs) => lhs.currency == rhs.currency ? new CurrencyAmount(lhs.amount + rhs.amount, lhs.currency) : throw new ArgumentException();
+    public static CurrencyAmount operator -(CurrencyAmount lhs, CurrencyAmount rhs) => lhs.currency == rhs.currency ? new CurrencyAmount(lhs.amount - rhs.amount, lhs.currency) : throw new ArgumentException();
+    public static CurrencyAmount operator *(CurrencyAmount lhs, CurrencyAmount rhs) => lhs.currency == rhs.currency ? new CurrencyAmount(lhs.amount * rhs.amount, lhs.currency) : throw new ArgumentException();
+    public static CurrencyAmount operator /(CurrencyAmount lhs, CurrencyAmount rhs) => lhs.currency == rhs.currency ? new CurrencyAmount(lhs.amount / rhs.amount, lhs.currency) : throw new ArgumentException();
 
     public static explicit operator double(CurrencyAmount c) => (double)c.amount;
     public static implicit operator decimal(CurrencyAmount c) => c.amount;
