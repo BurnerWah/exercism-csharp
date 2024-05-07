f116d99e6894b250a665af4d7e494723d75f8603
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-05-07 12:46:13 -0500

    Smplified code???

--- error-handling/ErrorHandling.cs
+++ error-handling/ErrorHandling.cs
@@ -3,25 +3,9 @@ using System;
 public static class ErrorHandling {
     public static void HandleErrorByThrowingException() => throw new Exception();
 
-    public static int? HandleErrorByReturningNullableType(string input) {
-        try {
-            return Int32.Parse(input);
-        }
-        catch (Exception) {
-            return null;
-        }
-    }
+    public static int? HandleErrorByReturningNullableType(string input) => Int32.TryParse(input, out var result) ? result : null;
 
-    public static bool HandleErrorWithOutParam(string input, out int result) {
-        try {
-            result = Int32.Parse(input);
-            return true;
-        }
-        catch (Exception) {
-            result = -1;
-            return false;
-        }
-    }
+    public static bool HandleErrorWithOutParam(string input, out int result) => Int32.TryParse(input, out result);
 
     public static void DisposableResourcesAreDisposedWhenExceptionIsThrown(IDisposable disposableObject) {
         disposableObject.Dispose();
