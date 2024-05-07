29093b4a79a2147f82adcfce1ce95fd33f738a45
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-05-07 12:24:13 -0500

    Cleaned up code

--- red-vs-blue-darwin-style/RedVsBlueDarwinStyle.cs
+++ red-vs-blue-darwin-style/RedVsBlueDarwinStyle.cs
@@ -1,66 +1,34 @@
 namespace RedRemoteControlCarTeam {
-    public class RemoteControlCar {
-        public RemoteControlCar(Motor motor, Chassis chassis, Telemetry telemetry, RunningGear runningGear) {
-        }
-        // red members and API
-    }
-
-    public class RunningGear {
-        // red members and API
-    }
-
-    public class Telemetry {
-        // red members and API
-    }
-
-    public class Chassis {
-        // red members and API
-    }
-
-    public class Motor {
-        // red members and API
-    }
+    public class RemoteControlCar(Motor motor, Chassis chassis, Telemetry telemetry, RunningGear runningGear) { }
+    public class RunningGear { }
+    public class Telemetry { }
+    public class Chassis { }
+    public class Motor { }
 }
 
 namespace BlueRemoteControlCarTeam {
-    public class RemoteControlCar {
-        public RemoteControlCar(Motor motor, Chassis chassis, Telemetry telemetry) {
-        }
-        // blue members and API
-    }
-
-    public class Telemetry {
-        // blue members and API
-    }
-
-    public class Chassis {
-        // blue members and API
-    }
-
-    public class Motor {
-        // blue members and API
-    }
+    public class RemoteControlCar(Motor motor, Chassis chassis, Telemetry telemetry) { }
+    public class Telemetry { }
+    public class Chassis { }
+    public class Motor { }
 }
 
 namespace Combined {
     using Red = RedRemoteControlCarTeam;
     using Blue = BlueRemoteControlCarTeam;
-    public static class CarBuilder {
-        public static Red.RemoteControlCar BuildRed() {
-            return new Red.RemoteControlCar(
-                new Red.Motor(),
-                new Red.Chassis(),
-                new Red.Telemetry(),
-                new Red.RunningGear()
-            );
-        }
 
-        public static Blue.RemoteControlCar BuildBlue() {
-            return new Blue.RemoteControlCar(
-                new Blue.Motor(),
-                new Blue.Chassis(),
-                new Blue.Telemetry()
-            );
-        }
+    public static class CarBuilder {
+        public static Red.RemoteControlCar BuildRed() => new Red.RemoteControlCar(
+            new Red.Motor(),
+            new Red.Chassis(),
+            new Red.Telemetry(),
+            new Red.RunningGear()
+        );
+
+        public static Blue.RemoteControlCar BuildBlue() => new Blue.RemoteControlCar(
+            new Blue.Motor(),
+            new Blue.Chassis(),
+            new Blue.Telemetry()
+        );
     }
 }
