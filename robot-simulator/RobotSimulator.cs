e7367711137052d6396751de61f8ba80e7ac4550
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-05-07 13:00:20 -0500

    Added robot simulator

---
+++ robot-simulator/RobotSimulator.cs
@@ -0,0 +1,35 @@
+using System;
+
+public enum Direction {
+    North,
+    East,
+    South,
+    West
+}
+
+public class RobotSimulator {
+    public RobotSimulator(Direction direction, int x, int y) {
+    }
+
+    public Direction Direction {
+        get {
+            throw new NotImplementedException("You need to implement this method.");
+        }
+    }
+
+    public int X {
+        get {
+            throw new NotImplementedException("You need to implement this method.");
+        }
+    }
+
+    public int Y {
+        get {
+            throw new NotImplementedException("You need to implement this method.");
+        }
+    }
+
+    public void Move(string instructions) {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+}
